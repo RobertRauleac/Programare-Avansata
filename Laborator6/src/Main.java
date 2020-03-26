42dc0525bc1d8ead25e5119c551cd0345659db18
RobertRauleac <48402944+RobertRauleac@users.noreply.github.com>
2020-03-26 13:44:16 +0200

    Add files via upload

---
+++ Laborator6/src/Main.java
@@ -0,0 +1,8 @@
+import geoshapes.DrawingFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new DrawingFrame().setVisible(true);
+    }
+}
---
+++ Laborator6/src/geoshapes/ControlPanel.java
@@ -0,0 +1,23 @@
+package geoshapes;
+
+import javax.swing.*;
+
+public class ControlPanel extends JPanel {
+    private final DrawingFrame frame;
+    JButton loadButton = new JButton("Load");
+    JButton saveButton = new JButton("Save");
+    JButton resetButton = new JButton("Reset");
+    JButton exitButton = new JButton("Exit");
+
+    public ControlPanel(DrawingFrame frame) {
+        this.frame = frame;
+        init();
+    }
+
+    private void init() {
+        add(loadButton);
+        add(saveButton);
+        add(resetButton);
+        add(exitButton);
+    }
+}
